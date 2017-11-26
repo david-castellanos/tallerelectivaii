fecd97c046e6fbe81a7c0fe5642971c453b7a2e6
guita <guita@DESKTOP-1ORFUQ9>
2017-11-25 20:14:34 -0500

    D

--- src/edu/uptc/taller/logic/Login.java
+++ src/edu/uptc/taller/logic/Login.java
@@ -14,9 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
+
     public Login() {
         // TODO Auto-generated constructor stub
     }
